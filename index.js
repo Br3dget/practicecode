49b37c843400930661dfe45560d5eb13d1a036bd
Br3dget <172260343+Br3dget@users.noreply.github.com.>
2026-05-01 01:17:48 +0300

    to be continued

---
+++ index.js
@@ -0,0 +1,15 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+
+}
+
+function calculate(){
+
+}
